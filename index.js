e0c77642a0e5643e476fff159c3760221de1f39c
Cheplel <jean.koech@student.moringaschool.com>
2026-04-09 02:05:33 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-function createLoginTracker(userInfo) {
-    const userInfo = () => {
-        username: user
-        password: password
+let burgers = [ "Hamburger", "Cheeseburger"];
+let featuredDrink =[ "Strawberry Milkshake"];
+
+function addBurger() {
+    let newburger = "Flatburger";
+    burgers.push(newburger);
+    let anotherNewBurger = "maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+
+    if(true) {
+        console.log("Burger added successfully!");
     }
 }
-const createLoginTracker = (userInfo) => {
-    createLoginTracker(userInfo)
-    let attemptCount = 0;
-    return function loginTracker() {
-        attemptCount++;
-        console.log(`Login attempt ${attemptCount} for user: ${userInfo.username}`);
-    }
-}
-const correctUserInfo = {
-    username: "admin",
-    password: "password123"
-    
-}
-if (userInfo.username === incorrectUserInfo.username && userInfo.password === incorrectUserInfo.password) {
- return "Login failed!"
-}
-else if (userInfo.username === incorrectUserInfo.username && userInfo.password === incorrectUserInfo.password) {
-    return "Login failed!"
-
-} 
-else if (attemptCount >= 3) {
-    return "Account locked due to multiple failed login attempts."
-}
-else {
-    return "Login successful!"
-}
-
-return loginTracker()
+function changeFeaturedDrink() {
+    let newFeaturedDrink = "The JavaShake";
+    featuredDrink[0] = newFeaturedDrink;}
